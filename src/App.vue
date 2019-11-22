c296343d6a66990d78b5c796d7c0bcae43f22d8f
Dmytro-Zabolotnyi <fuzzy.supernova@gmail.com>
2019-11-23 00:11:08 +0200

    Week 4 commit 1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
     <div id="app">
-        <input type="checkbox" id="slide-menu-checkbox" v-model="checked">
-        <label id="slide-menu-label" for="slide-menu-checkbox"></label>
-        <RightColumn v-on:changeNotificationCounter="changeNotificationCounter($event)" v-on:uncheckCheckbox="uncheckCheckbox"/>
+        <input
+                type="checkbox"
+                id="slide-menu-checkbox"
+                v-model="isSlideMenuActive"
+        >
+        <label
+                id="slide-menu-label"
+                for="slide-menu-checkbox"
+                v-bind:style="{ backgroundImage: 'url(' + slideMenuLabelIcon + ')' }">
+        </label>
+        <RightColumn
+                v-on:changeNotificationCounter="changeNotificationCounter($event)"
+                v-on:uncheckCheckbox="hideSlideMenu"
+        />
         <LeftColumn v-bind:notificationCounter="notificationCounter"/>
     </div>
 </template>
@@ -16,7 +27,8 @@
         components: {RightColumn, LeftColumn},
         data: function () {
             return {
-                checked: false,
+                isSlideMenuActive: false,
+                slideMenuLabelIcon: "images/chevron-right.svg",
                 notificationCounter: 3
             }
         },
@@ -24,31 +36,88 @@
             changeNotificationCounter: function (index) {
                 this.notificationCounter = index;
             },
-            uncheckCheckboxOnResize: function () {
-                if ((window.innerWidth / window.innerHeight) > (980/927)) {
-                    if (this.checked) {
-                        this.checked = !this.checked;
+            hideSlideMenuOnResize: function () {
+                if ((window.innerWidth / window.innerHeight) > (980 / 927)) {
+                    if (this.isSlideMenuActive) {
+                        this.isSlideMenuActive = !this.isSlideMenuActive;
                     }
                 }
             },
-            uncheckCheckbox: function () {
-                if (this.checked) {
-                    this.checked = !this.checked;
+            hideSlideMenu: function () {
+                if (this.isSlideMenuActive) {
+                    this.isSlideMenuActive = !this.isSlideMenuActive;
                 }
             }
         },
         mounted: function () {
             this.$nextTick(function () {
-                window.addEventListener('resize', this.uncheckCheckboxOnResize);
-                this.uncheckCheckboxOnResize();
+                window.addEventListener('resize', this.hideSlideMenuOnResize);
+                this.hideSlideMenuOnResize();
             })
         },
         beforeDestroy: function () {
-            window.removeEventListener('resize', this.uncheckCheckboxOnResize)
+            window.removeEventListener('resize', this.hideSlideMenuOnResize)
         }
     }
 </script>
 
 <style>
+    #app {
+        position: relative;
+        width: 100%;
+        height: 100%;
+    }
 
+    #slide-menu-label {
+        display: none;
+        position: absolute;
+        top: 50%;
+        left: 100%;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        background: #FFC200;
+        z-index: 1;
+        -webkit-transition: left 0.3s, box-shadow 0.3s, -webkit-transform 0.3s;
+        transition: left 0.3s, transform 0.3s, box-shadow 0.3s, -webkit-transform 0.3s;
+    }
+
+    #slide-menu-label:hover {
+        cursor: pointer;
+        box-shadow: 0 0 4px black;
+    }
+
+    #slide-menu-checkbox {
+        display: none;
+    }
+
+
+    #slide-menu-checkbox:checked ~ #slide-menu-label {
+        -webkit-transform: rotate(180deg);
+        transform: rotate(180deg);
+        left: 250px;
+    }
+
+    @media screen and (max-aspect-ratio: 980/927) {
+        #slide-menu-label {
+            display: block;
+            left: 30px;
+        }
+    }
+
+    @media screen and (max-aspect-ratio: 1/2), (max-aspect-ratio: 2/3) and (max-width: 415px) {
+        #slide-menu-checkbox:checked ~ #slide-menu-label {
+            left: 55.4%;
+        }
+
+        #slide-menu-label {
+            width: 8.71vw;
+            height: 8.71vw;
+            left: 1.6%;
+        }
+
+        #slide-menu-label:hover {
+            box-shadow: 0 0 1vw black;
+        }
+    }
 </style>
--- src/components/TasksContainer.vue
+++ src/components/TasksContainer.vue
@@ -3,8 +3,10 @@
         <div class="tasks">
             <div class="day"></div>
             <ul>
-                <li v-for="(task, taskIndex) in tasks" v-bind:key="taskIndex">
-                    <span v-for="(item, itemIndex) in task" v-bind:key="itemIndex">{{ item }}</span>
+                <li v-for="task in tasks" v-bind:key="task.id">
+                    <span>{{ task.name }}</span>
+                    <span>{{ task.description }}</span>
+                    <span>{{ task.deadline }}</span>
                 </li>
             </ul>
         </div>
@@ -17,17 +19,61 @@
         data: function () {
             return {
                 tasks: [
-                    ['Teeth', 'Take a toothbrush and make your breath fresh.', '6:35 AM'],
-                    ['Dog', 'Pet your dog and take it for a morning jog.', '7:10 AM'],
-                    ['Shower', 'Give yourself a pleasure of a morning shower.', '7:20 AM'],
-                    ['Breakfast', 'Cook and eat some bacon and eggs.', '7:50 AM'],
-                    ['Car', 'Grab your keys, head to the car and leave to work.', '8:20 AM'],
-                    ['Work', 'Be a good pal and make something worthy today.', '6:00 PM'],
-                    ['Date', 'Have a good time with a beautiful woman.', '9:00 PM'],
-                    ['Home', "Bring yourself home. And take your beautiful woman with you. She's your wife, after all.", '9:45 PM'],
-                    ['Evening', "You're a big boy, come up with something.", '11:30 PM'],
-                    ['Teeth', "Your dentist told you to brush your teeth twice a day, didn't he?", '11:35 PM'],
-                    ['Sleep', 'It was a good day, time to let it go and have some sleep.', '11:40 PM']
+                    {
+                        name: 'Teeth',
+                        description: 'Take a toothbrush and make your breath fresh.',
+                        deadline: '6:35 AM'
+                    },
+                    {
+                        name: 'Dog',
+                        description: 'Pet your dog and take it for a morning jog.',
+                        deadline: '7:10 AM'
+                    },
+                    {
+                        name: 'Shower',
+                        description: 'Give yourself a pleasure of a morning shower.',
+                        deadline: '7:20 AM'
+                    },
+                    {
+                        name: 'Breakfast',
+                        description: 'Cook and eat some bacon and eggs.',
+                        deadline: '7:50 AM'
+                    },
+                    {
+                        name: 'Car',
+                        description: 'Grab your keys, head to the car and leave to work.',
+                        deadline: '8:20 AM'
+                    },
+                    {
+                        name: 'Work',
+                        description: 'Be a good pal and make something worthy today.',
+                        deadline: '6:00 PM'
+                    },
+                    {
+                        name: 'Date',
+                        description: 'Have a good time with a beautiful woman.',
+                        deadline: '9:00 PM'
+                    },
+                    {
+                        name: 'Home',
+                        description: "Bring yourself home. And take your beautiful woman with you. She's your wife, after all.",
+                        deadline: '9:45 PM'
+                    },
+                    {
+                        name: 'Evening',
+                        description: "You're a big boy, come up with something.",
+                        deadline: '11:30 PM'
+                    },
+                    {
+                        name: 'Teeth',
+                        description: "Your dentist told you to brush your teeth twice a day, didn't he?",
+                        deadline: '11:35 PM'
+                    },
+                    {
+                        name: 'Sleep',
+                        description: 'It was a good day, time to let it go and have some sleep.',
+                        deadline: '11:40 PM'
+                    }
                 ]
             }
         }
@@ -35,6 +81,17 @@
 </script>
 
 <style scoped>
+    .day {
+        margin-bottom: 2px;
+    }
+
+    .day::before {
+        content: "TODAY";
+        opacity: 0.5;
+        font-size: 14px;
+        color: #131313;
+    }
+
     .tasks {
         width: 100%;
     }
@@ -42,12 +99,17 @@
     ul li {
         display: flex;
         flex-direction: row;
+        flex-wrap: wrap;
         justify-content: space-between;
         height: min-content;
         width: 100%;
-        margin: 30px 0;
+        margin: 5px 0;
         border-radius: 8px;
         box-sizing: border-box;
+    }
+
+    ul li:first-child {
+        margin-top: 30px;
     }
 
     ul li:last-child {
@@ -59,21 +121,16 @@
         font-size: 15px;
         line-height: 18px;
         color: #131313;
+        width: 100%;
     }
 
     span:nth-child(1), span:nth-child(3) {
-        flex: 1;
         font-size: 14px;
         color: #131313;
         line-height: 20px;
     }
 
-    span:nth-child(1) {
-        flex-basis: 70px;
-    }
-
     span:nth-child(2) {
-        width: 100%;
         background: #F7F6F3;
         border-radius: 10px;
         box-sizing: border-box;
@@ -87,11 +144,6 @@
         white-space: nowrap;
     }
 
-    span:nth-child(1) + span:nth-child(2),
-    span:nth-child(2) + span:nth-child(3) {
-        margin-left: 20px;
-    }
-
     @media screen and (max-aspect-ratio: 1/2) {
         .day {
             margin-bottom: 0.65vw;
@@ -99,6 +151,10 @@
 
         .day::before {
             font-size: 3vw;
+        }
+
+        ul li:first-child {
+            margin-top: 6.5vw;
         }
 
         span {
@@ -122,11 +178,6 @@
 
         span:nth-child(3) {
             white-space: normal;
-        }
-
-        span:nth-child(1) + span:nth-child(2),
-        span:nth-child(2) + span:nth-child(3) {
-            margin-left: 4.35vw;
         }
     }
 </style>
